1cab5ea69f395e86b2f2d670c5494b0e3d55cfec
Pushka <76700084+Pushkka@users.noreply.github.com>
2020-12-28 20:22:24 +0300

    Add files via upload

---
+++ Cover generator/Assets/Scripts/Cover.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cover : MonoBehaviour
+{
+    public CoverType TypeOfCover = CoverType.Stand;
+    public enum CoverType
+    {
+        Stand,
+        Duck
+    }
+    public BotController MyBot;
+    [SerializeField]
+    public bool Right;
+}
---
+++ Cover generator/Assets/Scripts/CoverSYS.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(CoverGenerator))]
+public class CoverSYS : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        CoverGenerator generator = (CoverGenerator)target;
+        if (GUILayout.Button("Generate Covers"))
+        {
+            generator.GenerateCovers();
+        }
+    }
+}
